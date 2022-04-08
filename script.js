b1be3cacd6b73d4e29cde73dd4ecd7287bb8ce32
Dayane <dayanecarolina.oliveira@gmail.com>
2022-04-08 17:05:07 -0300

    Versao teste

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
-const parrots= ["bobrossparrot","explodyparrot","fiestaparrot","metalparrot","revertitparrot","tripletsparrot","unicornparrot"];
-let numCards=prompt("Escolha um número par entre 4 e 14 para definir com quantas cartas deseja jogar!");
+let numCards=parseInt(prompt("Escolha um número par entre 4 e 14 para definir com quantas cartas deseja jogar!"));
+
+// const parrotsPicked=[];
 
 
-while (numCards<4||numCards>14||(numCards%2!==0)){
+
+while ((numCards%2 !== 0) || (numCards<4) || (numCards>14)){
     numCards = prompt("Por favor, insira um número válido! Escolha um número par entre 4 e 14");
+}
+const list = document.querySelector("ul");
+const parrots= ["bobrossparrot","explodyparrot","fiestaparrot","metalparrot","revertitparrot","tripletsparrot","unicornparrot"];
+parrots=parrots.sort(comparador);
+
+list.innerHTML = `
+    <li class="card">
+            <div class="content front on">
+                <img src="img/front.png">
+            </div>
+            <div class="content back off">
+                <img src="img/unicornparrot.gif">
+            </div>
+    </li>
+    `;
+// for (let j=0; j<(numCards/2);j++){
+//     parrotsPicked=parrotsPicked.push(parrots[j]);
+//     parrotsPicked=parrotsPicked.push(parrots[j]);
+//     console.log(parrotsPicked);
+// }
+
+// parrotsPicked=parrotsPicked.sort(comparador);
+// console.log(parrotsPicked);
+
+insertCards(numCards);
+
+// function insertCards(){
+
+// ISSO VAI NO FOR
+
+    
+
+
+// Esta função pode ficar separada do código acima, onde você preferir
+function comparador() { 
+	return Math.random() - 0.5; 
 }
