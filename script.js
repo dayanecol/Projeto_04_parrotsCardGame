5b9fde7eb8556ee837ab8ffea385db5a41d4c903
Dayane <dayanecarolina.oliveira@gmail.com>
2022-04-08 18:30:13 -0300

    Tentando fazer push 2

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ while ((numCards%2 !== 0) || (numCards<4) || (numCards>14)){
 const list = document.querySelector("ul");
 const parrots= ["bobrossparrot","explodyparrot","fiestaparrot","metalparrot","revertitparrot","tripletsparrot","unicornparrot"];
 parrots=parrots.sort(comparador);
+insertCards(numCards);
+
+function insertCards(numCards){
+
+    console.log(numCards);
+
+    list.innerHTML = `
+    <li class="card">
+        <div class="content front on">
+            <img src="img/front.png">
+        </div>
+        <div class="content back off">
+            <img src="img/unicornparrot.gif">
+        </div>
+    </li>
+    `;
+}
 
 
 
