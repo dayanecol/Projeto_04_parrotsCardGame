219a70315ab3975ea2bf73771d0dbce520d4f02e
Dayane <dayanecarolina.oliveira@gmail.com>
2022-04-09 15:18:34 -0300

    Versionamento clique na carta

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let parrots= ["bobrossparrot","explodyparrot","fiestaparrot","metalparrot","reve
 let numCards = 0;
 let deck = [];
 let card= ``;
+let time= 10;
+let pickedCards=[];
+let pairs;
+let first_card=``;
+let second_card=``;
 
 // const parrotsPicked=[];
 
@@ -29,16 +34,16 @@ function insertCards(){ //Inserir cartas dinamicamente no HTML
 }
 
 function buildDeck(){
-    let pairs =(numCards/2)
+    let numPairs =(numCards/2)
     
 
-    for (let j=0; j < pairs; j++){
+    for (let j=0; j < numPairs; j++){
         card = `
-        <li class="card">
-            <div class="content front on">
+        <li class="card" onclick="selectedCard(this)">
+            <div class="content front">
                 <img src="img/front.png"/>
             </div>
-            <div class="content back off">
+            <div class="content back">
                 <img src="img/${parrots[j]}.gif"/>
             </div>
         </li>
@@ -47,6 +52,59 @@ function buildDeck(){
         deck.push(card);
     }
 }
+
+function selectedCard(element){
+    let selected = element.classList.contains("selected");
+    let isPair = element.classList.contains("Pairs");
+    
+    
+    if (!selected || !isPair ){
+        
+        element.classList.add("selected");
+        pickedCards.push(element);
+        if (pickedCards.length === 2){
+            compareCards();
+        }
+        // setTimeout(turnOver(element), time*1000);
+    }
+    
+}
+
+function untap(){
+    pickedCards[0].classList.remove("selected"); 
+    pickedCards[1].classList.remove("selected"); 
+    pickedCards=[]
+
+}
+//     element.querySelector(".back").classList.add("off");
+//     element.querySelector(".front").classList.remove("off");
+
+// function delay(){
+//     turnOver(first_card);
+//     turnOver(second_card);
+
+//     pickedCards=[];
+    
+// }
+
+
+function compareCards(){
+    first_card = pickedCards[0];
+    second_card = pickedCards[1];
+
+    if (first_card.innerHTML===second_card.innerHTML){
+        first_card.removeAttribute("onclick");
+        first_card.classList.add("pairs");
+        second_card.removeAttribute("onclick");
+        second_card.classList.add("pairs");
+        pickedCards=[];
+    }else{
+        setTimeout(untap,1000);
+    }
+    
+
+}
+
 
 // Esta função pode ficar separada do código acima, onde você preferir
 function comparador() { 
